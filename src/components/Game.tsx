8cd8e1885ae91f950ac081e8593681354cec4edf
guraogr <guraogr@gmail.com>
2022-06-21 01:38:35 +0900

    feat: タイムトラベル機能の追加

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -6,9 +6,27 @@ import calculateWinner from '../helper/calculateWinner';
 const Game = () => {
   const [history, setHistory] = useState<History[]>([{ squares: Array(9).fill(null) }]);
   const [xIsNext, setXIsNext] = useState<boolean>(true);
+  const [stepNumber, setStepNumber] = useState<number>(0);
 
-  const current = history[history.length - 1];
+  const current = history[stepNumber];
   const winner = calculateWinner(current.squares);
+
+  const jumpTo = (step: number) => {
+    setStepNumber(step);
+    setXIsNext(step % 2 === 0);
+  };
+
+  const moves = history.map((step, move) => {
+    const desc = move ? `Got to move ${move}` : 'Go to game start';
+    return (
+      <li key={move}>
+        <button type="button" onClick={() => jumpTo(move)}>
+          {desc}
+        </button>
+      </li>
+    );
+  });
+
   let status;
   if (winner) {
     status = `Winner: ${winner}`;
@@ -17,12 +35,14 @@ const Game = () => {
   }
 
   const handleClick = (i: number) => {
+    const newHistory = history.slice(0, stepNumber + 1);
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
     squares[i] = xIsNext ? 'X' : 'O';
-    setHistory(history.concat([{ squares }]));
+    setHistory(newHistory.concat([{ squares }]));
+    setStepNumber(newHistory.length);
     setXIsNext(!xIsNext);
   };
 
@@ -33,7 +53,7 @@ const Game = () => {
       </div>
       <div className="game-info">
         <div>{status}</div>
-        <ol>{/* TODO */}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
