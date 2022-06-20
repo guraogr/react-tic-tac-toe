cb0886fb049afc104a4c78c1ca040731157f58e1
guraogr <guraogr@gmail.com>
2022-06-20 22:30:48 +0900

    feat: bordコンポーネントの作成

---
+++ src/components/Board.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import Square from './Square';
+
+const Board = () => {
+  const [squares, setSquares] = useState<string[] | null[]>(Array(9).fill(null));
+  const onSquareClick = (i: number) => {
+    const copySquares = squares.slice();
+    copySquares[i] = 'X';
+    setSquares(copySquares);
+  };
+  const renderSquare = (i: number) => <Square value={squares[i]} onClick={() => onSquareClick(i)} />;
+  const status = 'Next player: X';
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+};
+
+export default Board;
